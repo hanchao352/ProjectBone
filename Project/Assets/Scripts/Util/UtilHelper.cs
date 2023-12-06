28f76420be0267310cc92ea6af31ce4565acbc60
hanchao352 <352562274@qq.com>
2023-12-06 22:52:23 +0800

    删除无用协议和方法

--- Project/Assets/Scripts/Util/UtilHelper.cs
+++ Project/Assets/Scripts/Util/UtilHelper.cs
@@ -8,18 +8,7 @@ using Google.Protobuf;
 
 public static class UtilHelper
 {
-        public static RoleInfo ToRoleInfo(this NRoleInfo roleInfo)
-        {
-            return new RoleInfo()
-            {
-                AccountId = roleInfo.Accountid,
-                RoleId = roleInfo.RoleId,
-                RoleName = roleInfo.Name,
-                Level = roleInfo.Level,
-                BigServerId = roleInfo.BigServerId,
-                SmallServerId = roleInfo.SmallServerId
-            };
-        }
+       
         
        
 }
